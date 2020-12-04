8be8e2ff303f702b4460499170d19496e704a7e6
jfrank <jfrank2019@163.com>
2020-12-04 16:00:32 +0800

    路由切换之后关闭 aside

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <router-view></router-view>
+  <router-view />
 </template>
 
 <script lang="ts">
 import { provide, ref } from 'vue'
+import router from './router'
 export default {
   name: 'App',
   setup() {
     const width = document.documentElement.clientWidth
     const asideVisible = ref(width > 500)
     provide('asideVisible', asideVisible) // set
+    router.afterEach(() => {
+      if (width <= 500) {
+        asideVisible.value = false
+      }
+    })
   }
 }
 </script>
---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>switch-demo 文档</div>
+</template>
