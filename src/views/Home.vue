02cd3136c1d1251289ee3ebf3524937244d65b80
jfrank <jfrank2019@163.com>
2020-12-04 08:29:25 +0800

    初始化 aside 的html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>咕噜UI</h1>
       <h2>一个厉害的 UI 框架</h2>
       <p class="actions">
-        <a href="">Github</a>
-        <a href="">开始</a>
+        <a href="https://github.com">Github</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -20,20 +20,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.topnav {
-  background: pink;
+.banner {
+  padding: 100px 0;
   display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
+  justify-content: center;
+  flex-direction: column;
+  background: lightgreen;
+  text-align: center;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
     }
   }
 }
