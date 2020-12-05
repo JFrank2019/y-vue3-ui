43fdd4545bc2f77e405792e6fb010fcc066fcec2
jfrank <jfrank2019@163.com>
2020-12-05 21:04:07 +0800

    开关按钮组件动画完成

---
+++ src/lib/Switch.vue
@@ -0,0 +1,49 @@
+<template>
+  <button :class="{ checked: checked }" @click="toggle"><span></span></button>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const checked = ref(false)
+    const toggle = () => {
+      checked.value = !checked.value
+    }
+    return { checked, toggle }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: grey;
+  border-radius: $h / 2;
+  position: relative;
+  &:focus {
+    outline: none;
+  }
+  span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h / 2;
+    transition: left 0.25s;
+  }
+  &.checked {
+    background: blue;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+}
+</style>
