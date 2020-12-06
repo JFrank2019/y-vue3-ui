d1aeae2c4ecabf9d6e1094d6d6e3aedb56d1ef09
jfrank <jfrank2019@163.com>
2020-12-06 16:22:12 +0800

    Dialog Css1

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>Dialog 的文档</div>
+  <div>Dialog 示例</div>
+
+  <h1>示例1</h1>
+  <Button @click="toggle">toggle</Button>
+  <Dialog :visible="x" />
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import { ref } from 'vue'
+export default {
+  components: { Dialog, Button },
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
+    }
+    return { x, toggle }
+  }
+}
+</script>
