04f958986b3fdb204981d45632fcee5c025f3c5a
jfrank <jfrank2019@163.com>
2020-12-07 14:25:01 +0800

    优化1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -9,7 +9,7 @@
         :key="index"
         :ref="
           el => {
-            if (el) navItems[index] = el
+            if (t === selected) selectedItem = el
           }
         "
       >
@@ -38,17 +38,15 @@ export default {
     }
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([])
+    const selectedItem = ref<HTMLDivElement>(null)
     const indicator = ref<HTMLDivElement>(null)
     const container = ref<HTMLDivElement>(null)
     const x = () => {
-      const divs = navItems.value
-      const result = divs.filter(div => div.classList.contains('selected'))[0]
-      const { width } = result.getBoundingClientRect()
+      const { width } = selectedItem.value.getBoundingClientRect()
       indicator.value.style.width = width + 'px'
 
       const { left: left1 } = container.value.getBoundingClientRect()
-      const { left: left2 } = result.getBoundingClientRect()
+      const { left: left2 } = selectedItem.value.getBoundingClientRect()
       const left = left2 - left1
       indicator.value.style.left = left + 'px'
     }
@@ -77,9 +75,9 @@ export default {
       titles,
       current,
       select,
-      navItems,
       indicator,
-      container
+      container,
+      selectedItem
     }
   }
 }
